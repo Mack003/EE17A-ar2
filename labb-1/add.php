8eb00a9c7afc959f1ea85aa548f7a68f32d50533
Marcus Åkerman <marcus.akerman@learnet.se>
2020-04-23 14:04:37 +0200

    123

--- labb-1/add.php
+++ labb-1/add.php
@@ -3,34 +3,28 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Labb 1</title>
+    <title>Skapa Inlägg</title>
     <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
 </head>
 <body>
     <div class="kontainer">
-    <h1>Lägg till restaurang</h1>
+    <h1>Lägg till inlägg</h1>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
     <label>Namn</label>
-    <input type="text" name="namn" required>
-    <label>Gata</label>
-    <input type="text" name="gata" required>
-    <label>Postnr</label>
-    <input type="text" name="postnr" required>
-    <label>Adress</label>
-    <input type="text" name="adress" required>
+    <input type="text" name="namn" required> <br>
+    <label>Inlägg</label>
+    <textarea name="inlagg" id="inlagg" cols="30" rows="10"></textarea>
     <button>Skicka</button>
         <?php
         /* Läs in filen */
-        $filnamn = './restauranger.csv';
+        $filnamn = './inlagg.csv';
 
         $namn = filter_input(INPUT_POST, 'namn', FILTER_SANITIZE_STRING);
-        $gata = filter_input(INPUT_POST, 'gata', FILTER_SANITIZE_STRING);
-        $postnr = filter_input(INPUT_POST, 'postnr', FILTER_SANITIZE_NUMBER_INT);
-        $adress = filter_input(INPUT_POST, 'adress', FILTER_SANITIZE_STRING);
+        $inlagg = filter_input(INPUT_POST, 'inlagg', FILTER_SANITIZE_STRING);
 
-        if ($namn && $gata && $postnr && $adress) {
+        if ($namn && $inlagg) {
             $fil = fopen($filnamn, 'a');
-            fwrite($fil, "\n$namn, $gata, $postnr, $adress");
+            fwrite($fil, "\n$namn ¨ $inlagg");
             fclose($fil);
         }
         ?>
--- labb-1/list.php
+++ labb-1/list.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Labb 1</title>
+    <title>Lista Inlägg</title>
     <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
 </head>
 <body>
@@ -11,19 +11,19 @@
         <?php
 
 /* Läs filen */
-$filename = './restauranger.csv';
+$filename = './inlagg.csv';
 if (is_readable($filename)) {
 
     $rader = file($filename);
     echo "<table>";
-    echo "<tr><th>Namn</th><th>Gata</th><th>Postnr</th><th>Adress</th></tr>";
+    echo "<tr><th>Namn</th><th>Inlägg</th></tr>";
     /* dela upp texten */
     foreach ($rader as $rad) {
 
-        $delar = explode(', ', $rad);
+        $delar = explode('¨ ', $rad);
 
         /* Skriv ut tabellrad */
-        echo "<tr><td>$delar[0]</td><td>$delar[1]</td><td>$delar[2]</td><td>$delar[3]</td></tr>";
+        echo "<tr><td>$delar[0]</td><td>$delar[1]</td></tr>";
     }
     echo "</table>";
 
