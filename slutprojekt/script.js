52b2230e5588ba7fcea814950120809ba6421596
Marcus Åkerman <marcus.akerman@learnet.se>
2020-05-15 10:52:26 +0200

    Påbörjat arbete av slutprojekt

---
+++ slutprojekt/script.js
@@ -0,0 +1,9 @@
+/* Element vi arbetar med */
+const eCanvas = document.querySelector("canvas");
+
+/* Ställ in bredd och storlek */
+eCanvas.width = 800;
+eCanvas.height = 600;
+
+/* Starta canvas rityta */
+var ctx = eCanvas.getContext("2d");
